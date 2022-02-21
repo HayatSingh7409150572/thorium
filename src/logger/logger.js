9b29f9f26da01cd95a36a926eccf265c2e7a2ba3
HayatSingh7409150572 <jeet53316@gmail.com>
2022-02-22 00:09:22 +0530

    jafar

---
+++ src/logger/logger.js
@@ -0,0 +1,8 @@
+
+function welcome()
+{
+    console.log("‘Welcome to my application. I am hayat singh and a part of FunctionUp Thorium cohor");
+}
+
+
+module.exports.welcome=welcome;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,17 @@
+
+let obj=require('../logger/logger');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+   // res.send('My first ever api!')
+    //obj.printMessage('thorium');
+    obj.welcome()
+    //console.log(obj.welcome);
+
     res.send('My first ever api!')
 });
 
 module.exports = router;
-// adding this comment for no reason
+// adding this comment for no reasonnc
