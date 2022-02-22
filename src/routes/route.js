e459f44200da2c9507f785a19ddce99d2ad4415d
HAYAT_SINGH <72182653+HayatSingh7409150572@users.noreply.github.com>
2022-02-23 00:25:29 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,17 +1,33 @@
+const obj =require('../logger/logger');
+ const obj1=require('../util/helper')
+ const obj2=require('../validator/formatter')
+const lodash = require('lodash')
 
-let obj=require('../logger/logger');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-   // res.send('My first ever api!')
-    //obj.printMessage('thorium');
-    obj.welcome()
-    //console.log(obj.welcome);
-
-    res.send('My first ever api!')
+    obj.welcome('Welcome to my application. I am Mohd jafar and a part of FunctionUp Thorium cohort.')
+    obj1.printDate();
+    obj1.printMonth();
+    obj1.getBatchInfo();
+    console.log(obj2.stringTrim('  functionUp   '));
+    console.log(obj2.lowerCase('fUNCTionUp'));
+    console.log(obj2.upperCase('functionUp'));
+        
+ res.send('My first ever api!')
+});
+router.get('/hello', function (req, res) {
+    console.log(lodash.chunk(['JAN','FEB','MARCH','APRIL','MAY','JUNE','JULY','AUGUST','SEP','OCT','NOV','DEC'],3))
+    console.log(lodash.tail([1,3,5,7,9,11,13,15,17,19]))
+    console.log(lodash.union([2],[1,2],[3,3,4,2],[9,8,4,3],[1,1,2,2,3]))
+    console.log(lodash.fromPairs([['horror','The Shining',],['drama','Titanic'],['thriller','Shutter Island'],['fantasy','Pans Labyrinth']]))
+  
+    res.send('My second ever api!')
 });
 
+
+
 module.exports = router;
-// adding this comment for no reasonnc
+// adding this comment for no reason../
