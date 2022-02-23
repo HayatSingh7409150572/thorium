84bf568275b2d9265214043ffef56cd1a0122d6c
HayatSingh7409150572 <jeet53316@gmail.com>
2022-02-24 00:52:12 +0530

    hhh

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,51 +1,170 @@
+const { application } = require('express');
 const express = require('express');
 
 const router = express.Router();
 
 
 
-router.get('/movies', function (req, res) {
-   res.send('["Fukrey","Delhi","Dabang","Rockstar","suryawansh"]')
-});
+//router.get('/movies', function (req, res) {
+   //res.send('["Fukrey","Delhi","Dabang","Rockstar","suryawansh"]')
+//})
+
+//Assignment1
+ router.post('/players', function(req, res){
 
 
-router.get('/movies/:movieId', function(req,res){
-mov =["Fukrey","Delhi","Dabang","Rockstar","suryawansh"]
- let value = req.params.movieId;
- if(value>mov.length-1)
- {
-     res.send("doesnt exist")
- }
- else
- { res.send(mov[value])
+let players  = [ {
 
- }
-})
+            "name": "manish",
+  
+             "dob": "1/1/1995",
+  
+              "gender": "male",
+  
+                "city": "jalandhar",
+  
+                  "sports": [
+  
+                    "swimming"
+  
+                          ],
+  
+                   "bookings": [
+  
+   
+                                 ]   },
 
-router.get('/moviez',function(req,res){
-   res.send([ {id: 1,name:'The Shining' }, {id: 2, name:'Incendies' }, {id: 3,name: 'Rang de Basanti'}, {id: 4, name: 'welcome' },{id: 5, name: 'Finding Demo' }])
-})
+                                            
+          {
 
-router.get('/films/:filmId', function(req,res){
-   let movie=[ {id: 1,name:'The Shining' }, {id: 2, name:'Incendies' },{id: 3,name: 'Rang de Basanti'}, {id: 4, name: 'welcome' },{id: 5, name: 'Finding Demo' }]
-    let value =req.params.filmId;
-    let found=false;
-    for(let i=0;i<movie.length;i++)
-    {
-        if(movie[i].id==value){
-            found= true
-            res.send(movie[i])
-            break
-        }
+             "name": "jafar",
+  
+    "dob": "1/1/1985",
+  
+    "gender": "male",
+  
+    "city": "lucknow",
+  
+    "sports": [
+  
+      "football"
+  
+    ],
+  
+    "bookings": [
+  
+   
+    ]
+  
+  },
+  {
+
+    "name": "hayat",
+  
+    "dob": "24/8/1999",
+  
+    "gender": "male",
+  
+    "city": "roorkee",
+  
+    "sports": [
+  
+      "cricket"
+  
+    ],
+  
+    "bookings": [
+  
+   
+    ]
+  
+  },
+  {
+
+    "name": "khusboo",
+  
+    "dob": "14/06/1998",
+  
+    "gender": "female",
+  
+    "city": "Amrawati",
+  
+    "sports": [
+  
+      "swimming"
+  
+    ],
+  
+    "bookings": [
+  
+   
+    ]
+  
+  },
+  {
+
+    "name": "shielesh pandit",
+  
+    "dob": "1/1/1996",
+  
+    "gender": "male",
+  
+    "city": "banaras",
+  
+    "sports": [
+  
+      "shooting"
+  
+    ],
+  
+    "bookings": [
+  
+   
+    ]
+  
+  }
+  
+  
+  ]
+
+
+let name=req.body.name
+players.push(name)
+
+let dob=req.body.dob
+players.push(dob)
+
+
+let gender=req.body.gender
+players.push(gender)
+
+let city=req.body.city
+players.push(city)
+
+let sports=req.body.sports
+players.push(sports)
+
+
+let bookings=req.body.bookings
+players.push(bookings)
+
+for (let i =0; i < players.length; i++)  {
+    if(players[i].name ==name){
+        console.log("playerexist")
+        res.send("player already exist")
+    }else{
+        players.push(name, dob, gender, city, sports, bookings)
     }
-    if(found == false)
-    {
-        res.send("No movie exits with this id ")
-    }
-
-})
+  }
 
 
+  res.send( {msg: players, status:true} )
+  });
 
-module.exports = router;
-// adding this comment for no reason../
+  
+
+
+
+
+
+module.exports =router;
