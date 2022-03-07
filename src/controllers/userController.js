07dd900b005f3dbe474ae21f9376539f9240655b
hayat singh <jeet53316@gmail.com>
2022-03-08 04:19:23 +0530

    hhh

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,30 @@
-const UserModel= require("../models/userModel")
+/*const UserModel= require("../models/userModel")
 
 const createUser= async function (req, res) {
     let data= req.body
     let savedData= await UserModel.create(data)
-    console.log(req.newAtribute)
     res.send({msg: savedData})
+}
+
+const getUsersData= async function (req, res) {
+    let allUsers= await UserModel.find()
+    res.send({msg: allUsers})
+}
+*/
+
+
+const UserModel= require("../models/userModel")
+
+const createUser= async function (req, res) {
+
+   let isUser = req.headers["isfreeappuser"]
+    let data= req.body
+   data.isFreeAppUser = isUser
+
+    let savedData= await UserModel.create(data)
+  
+    res.send({msg: savedData})
+
 }
 
 const getUsersData= async function (req, res) {
@@ -12,6 +32,32 @@ const getUsersData= async function (req, res) {
     console.log(req.newAtribute)
     res.send({msg: allUsers})
 }
+
+module.exports.createUser = createUser
+module.exports.getUsersData = getUsersData
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports.createUser= createUser
 module.exports.getUsersData= getUsersData
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,24 +1,29 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
+    
+        name:String,
+        balance:{
+            type:Number,
+            default:100,
+        },
+        address:String,
+        age:Number,
+         gender:{
 
-        required: true
-    },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
+             type:String,
+
+             enum:["male","female","other"],
+         },
+         
+        isFreeAppUser: {
+            type:Boolean,
+            default:false 
+        }
+    
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('UserM', userSchema) 
 
 
 
-// String, Number
-// Boolean, Object/json, array
